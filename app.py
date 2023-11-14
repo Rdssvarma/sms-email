112421bf7ff221415e659a7e3b145c8c9b88f2b2
DurgaSivaSaiVarmaRudraraju <saivarma3157@gmail.com>
2023-11-14 17:09:11 -0500

    s in message

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+input_sms = st.text_area("Enter the messages")
 
 if st.button('Predict'):
 
